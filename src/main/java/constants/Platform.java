b7d3f62918143015a5d5623e3da9c1a341d76767
Abraham Brian Juan <ajuan@PHMAMITFS01.infor.com>
2019-02-06 13:11:37 +0800

    updated Platform.java

--- src/main/java/constants/Platform.java
+++ src/main/java/constants/Platform.java
@@ -5,5 +5,6 @@ public final class Platform {
     //BrowserStack
     public static final String BROWSERSTACK_URL = "http://" + Credentials.BS_USERNAME_BRIAN +
             ":" + Credentials.BS_ACCESS_KEY_BRIAN + "@hub-cloud.browserstack.com/wd/hub";
+
     public static final String NAME = "browserstack";
 }
